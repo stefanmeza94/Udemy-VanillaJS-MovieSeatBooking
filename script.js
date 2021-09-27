feaf76b84e1ec28397ebd34853a215956a2c39b1
Stefan Meza <stefanmeza93@gmail.com>
2021-09-27 16:51:46 +0200

    functionality done

---
+++ script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// movie select event
+movieSelect.addEventListener('change', function(e) {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat click events
+container.addEventListener('click', function(e) {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
